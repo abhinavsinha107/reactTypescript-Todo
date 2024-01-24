0037924080a815b89cb1695ce85f394f9c50df0f
Salony Gupta <abhinavsinha135@gmail.com>
2024-01-24 11:15:48 +0530

    edit feature added

--- src/components/singleTodo/SingleTodo.tsx
+++ src/components/singleTodo/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../../model";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
@@ -14,21 +14,49 @@ type Props = {
 const SingleTodo = (props: Props) => {
   const { todo, todos, setTodos } = props;
 
-    const handleDone = (id: number) => {
-        setTodos(todos.map((todo) => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo))
-    }
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [editedTodo, setEditedTodo] = useState<string>(todo.todo);
 
-    const handleDelete = (id: number) => {
-        setTodos(todos.filter((todo) => todo.id !== id))
-    }
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
 
-  return (
-    <li className={styles.singleTodo}>
-      <span className={todo.isCompleted ? styles.done : ""}>
-        {todo.todo}
-      </span>
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, todo: editedTodo } : todo
+      )
+    );
+    setEditMode(false);
+  };
+
+  return editMode ? (
+    <form
+      className={styles.singleTodo}
+      onSubmit={(e) => handleEdit(e, todo.id)}
+    >
+      <input
+        type="text"
+        value={editedTodo}
+        onChange={(e) => setEditedTodo(e.target.value)}
+      />
       <div>
-        <FaEdit className={styles.icons} />
+        <FaEdit className={styles.icons} onClick={() => setEditMode(false)} />
+      </div>
+    </form>
+  ) : (
+    <li className={styles.singleTodo}>
+      <span className={todo.isCompleted ? styles.done : ""}>{todo.todo}</span>
+      <div>
+        <FaEdit className={styles.icons} onClick={() => setEditMode(true)} />
         <MdDelete
           className={styles.icons}
           onClick={() => handleDelete(todo.id)}
